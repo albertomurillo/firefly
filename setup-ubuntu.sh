88ffc86f485f1c7f3ecd0b9c4a200296dec6d8bd
Alberto Murillo <albertomurillosilva@gmail.com>
2018-07-22 23:06:23 -0700

    Add setup-ubuntu.sh
    - Run setup-ubuntu.sh on new ubuntu box
    - Prefer community roles
    - Drop WSL
    - Remove some packages
    
    Signed-off-by: Alberto Murillo <albertomurillosilva@gmail.com>

---
+++ setup-ubuntu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+which git > /dev/null
+if [[ $? != 0 ]]; then
+  echo "Installing git"
+  sudo apt install git -y
+fi
+
+if [[ ! -d ~/github ]]; then
+  echo "Creating $HOME/github"
+  mkdir ~/github
+fi
+
+if [[ ! -d ~/github/firefly ]]; then
+  echo "Cloning firefly repo"
+  git clone https://github.com/albertomurillo/firefly ~/github/firefly
+fi
+
+which ansible > /dev/null
+if [[ $? != 0 ]]; then
+  echo "Installing ansible"
+  sudo apt install ansible -t
+fi
+
+cd ~/github/firefly
+sudo ansible-galaxy install -r requirements.yml
+ansible-playbook main.yml
