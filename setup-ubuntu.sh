05fb33345484d4b00ea07834170e753467561594
Alberto Murillo <albertomurillosilva@gmail.com>
2018-07-24 12:32:15 -0700

    More updates
    Signed-off-by: Alberto Murillo <albertomurillosilva@gmail.com>

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -2,24 +2,24 @@
 
 which git > /dev/null
 if [[ $? != 0 ]]; then
-  echo "Installing git"
-  sudo apt install git -y
+    echo "Installing git"
+    sudo apt install git -y
 fi
 
 if [[ ! -d ~/github ]]; then
-  echo "Creating $HOME/github"
-  mkdir ~/github
+    echo "Creating $HOME/github"
+    mkdir ~/github
 fi
 
 if [[ ! -d ~/github/firefly ]]; then
-  echo "Cloning firefly repo"
-  git clone https://github.com/albertomurillo/firefly ~/github/firefly
+    echo "Cloning firefly repo"
+    git clone https://github.com/albertomurillo/firefly ~/github/firefly
 fi
 
 which ansible > /dev/null
 if [[ $? != 0 ]]; then
-  echo "Installing ansible"
-  sudo apt install ansible -t
+    echo "Installing ansible"
+    sudo apt install ansible -t
 fi
 
 cd ~/github/firefly
